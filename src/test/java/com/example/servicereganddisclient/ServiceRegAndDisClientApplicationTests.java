74c4a098a7e7a231cacae2c658699e4c2cb16ad8
khoanxl918@gmail.com <khoanxl918@gmail.com>
2020-06-08 10:37:31 +0700

    first commit

---
+++ src/test/java/com/example/servicereganddisclient/ServiceRegAndDisClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicereganddisclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegAndDisClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
